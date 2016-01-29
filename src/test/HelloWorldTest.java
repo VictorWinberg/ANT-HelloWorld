96aac3f2d51641a5b6dbeaa12bfc72143c0a7f6c
VictorWinberg <dat14vwi@student.lu.se>
2016-01-29 17:47:14 +0100

    ANT buildscript working

---
+++ src/test/HelloWorldTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+
+	@Test
+	public void testPrintStartTime() {
+		assertEquals(1,1);
+		System.out.println("Test");
+	}
+
+	@Test
+	public void testPrintFinishTime() {
+		assertEquals("12", "12");
+	}
+
+	@Test
+	public void testPrintResult() {
+		assertEquals(0, 0);
+	}
+}
